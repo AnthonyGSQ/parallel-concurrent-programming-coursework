0baf92bf36c45abf234fd098f3b398304ba37247
Anthony Sanchez <anthony.sanchezquiros@ucr.ac.cr>
2025-03-15 15:24:13 -0600

    Commented_examples finished

---
+++ pthreats/commented_examples/src/hello.c
@@ -0,0 +1,36 @@
+// Copyright 2025 Anthony Sanchez
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* greet(void* data);
+// procedure main:
+int main(void) {
+  // create_thread(greet)
+  pthread_t thread;
+  // se inicializa el thread, seguido van los atributos, luego
+  // la tarea que quiero que haga
+
+  // el retorno int es un codigo que indica si hubo error en la subrutina
+  int error = pthread_create(&thread,  /* attr */  NULL, greet,
+    /*args*/  NULL);
+  if (error == EXIT_SUCCESS) {
+    printf("Hello from main thread\n");
+    pthread_join(thread,  /*value_ptr*/  NULL);
+  } else {
+    // stderr imprime en un archivo de errores, es buena practica
+    // de programacion
+    fprintf(stderr, "Error: could not create secondary thread\n");
+  }
+  // print "Hello from main thread"
+  return error;
+}
+// end procedure
+
+// un nuevo hilo de ejecucion que trabajara independientemente del hilo main
+// procedure greet:
+void* greet(void* data) {
+  // print "Hello from secondary thread"
+  printf("Hello from secondary thread\n");
+  return NULL;
+}  // end procedure
