f9598c96daa20463d09e20b5311baa19effad909
Anthony Sanchez <anthony.sanchezquiros@ucr.ac.cr>
2025-03-18 11:18:37 -0600

    Agregando doxyfile

--- pthreats/invert/src/solution.c
+++ pthreats/invert/src/solution.c
@@ -10,15 +10,19 @@
  * @return Status code to the operating system, 0 means success.
  */
 int main(void) {
-  long numbers[1000];
-  size_t count = 0;
-  while(scanf("%ld", &numbers[count])==1){
-    count++;
+  long* numbers = malloc(1000*sizeof(long));
+  size_t count = 0, capacity = 1000;
+  while (scanf("%ld", &numbers[count]) == 1) {
+    if (count == capacity) {
+      capacity *= 10;
+      numbers = realloc(numbers, capacity* sizeof(long));
+    }
   }
 
-  for(long index=count-1;index>=0;--index){
+  for (long index=count-1; index >=0; --index) {
     printf("%ld", numbers[index]);
     printf("\n");
   }
+  free(numbers);
   return EXIT_SUCCESS;
 }
