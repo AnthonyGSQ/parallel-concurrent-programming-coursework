45dee281b5cf0727d8801a9c122889469e7a4b75
Anthony Sanchez <anthony.sanchezquiros@ucr.ac.cr>
2025-03-24 16:43:05 -0600

    Task hellow_iwi_pri finished

--- pthreats/hello_iw_pri/src/hello_iw_pri.c
+++ pthreats/hello_iw_pri/src/hello_iw_pri.c
@@ -7,8 +7,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-//estructura privada para los threads
+// estructura de los datos privados de cada hilo
 typedef struct private_data {
+  // se utilizan los uint64_t para que el size de las variables
+  // no dependa de la arquitectura de la computadora
   uint64_t thread_number;
   uint64_t thread_count;
   struct private_data* next;
@@ -40,10 +42,19 @@ int main(int argc, char* argv[]) {
 int create_threads(uint64_t thread_count) {
   int error = EXIT_SUCCESS;
   // for thread_number := 0 to thread_count do
+  // Reservamos espacio en la memoria con el size de un hilo * el numero
+  // de hilos que se van a crear
   pthread_t* threads = (pthread_t*) malloc(thread_count * sizeof(pthread_t));
+  // se usa calloc en private_data ya que a diferencia de malloc,
+  // calloc inicializa los datos, limpiando la "basura" que habia
+  // en la parte de la memoria reservada
   private_data_t* private_data = (private_data_t*)
     calloc(thread_count, sizeof(private_data_t));
+    // aseguramos que se hayan creado threads y private_data
+    // si no es el caso, se informa que no fue posible crear
+    // dichos datos
   if (threads && private_data) {
+    // for encargado de crear todos los hilos solicitados
     for (uint64_t thread_number = 0; thread_number < thread_count
         ; ++thread_number) {
       private_data[thread_number].thread_number = thread_number;
@@ -51,6 +62,7 @@ int create_threads(uint64_t thread_count) {
       // create_thread(greet, thread_number)
       error = pthread_create(&threads[thread_number], /*attr*/ NULL, greet
         , /*arg*/ &private_data[thread_number]);
+        // if para tratar el caso en donde el hilo no pudo ser creado
       if (error == EXIT_SUCCESS) {
       } else {
         fprintf(stderr, "Error: could not create secondary thread\n");
@@ -61,12 +73,12 @@ int create_threads(uint64_t thread_count) {
 
     // print "Hello from main thread"
     printf("Hello from main thread\n");
-
+    // bucle para unir todos los hilos creados
     for (uint64_t thread_number = 0; thread_number < thread_count
         ; ++thread_number) {
       pthread_join(threads[thread_number], /*value_ptr*/ NULL);
     }
-
+    // liberamos toda la memoria reservada
     free(private_data);
     free(threads);
   } else {
