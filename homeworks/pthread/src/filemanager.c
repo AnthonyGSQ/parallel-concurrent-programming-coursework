5078079316de81cc498c0cd6101809cdafd77531
Anthony Sanchez <anthony.sanchezquiros@ucr.ac.cr>
2025-04-09 19:56:30 -0600

    Tarea02: Moving function report_file to filemanager.c and adding fileobj parameter to lamina functions

--- homeworks/pthread/src/filemanager.c
+++ homeworks/pthread/src/filemanager.c
@@ -1,8 +1,56 @@
 // Copyright 2025 Anthony Sanchez
 #include "lamina.h"
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
+/**
+ * @brief Genera y guarda un archivo de reporte en formato TSV con la
+ * información de la simulación de la lámina.
+ *
+ * Esta función crea o abre un archivo TSV donde se almacenan los datos de la
+ * simulación, vease el nombre del binario, tiempo transcurrido, epsilon,
+ * height y conductivity Si no se puede abrir el archivo, se crea uno nuevo en
+ * una ruta alternativa (dentro de la carpeta job###).
+ * Además, se formatea el tiempo en formato legible para una persona
+ * 
+ * @param lamina Un puntero a la estructura lamina
+ *
+ * @return EXIT_SUCCESS Si el reporte fue exitoso, EXIT_FAILURE si no
+ *
+ * @note Esta función intenta abrir un archivo en la ruta proporcionada por la
+ * estructura `Lamina`. Si no se puede abrir, intentará crear un archivo en una
+ * ruta alternativa.
+ */
+int report_file(Lamina *lamina, shared_file_data *fileobj) {
+    char text[260] = "";
+    char temp[260] = "";
+    char file_name[260] = "";
+    char alternative_route[260] = "";
+    snprintf(file_name, sizeof(file_name), "%s", fileobj->binary_file_name);
+    char *lastSLash = strrchr(file_name, '/') + 1;
+    char *dot = strrchr(file_name, '.');
+    *dot = '\0';
+    snprintf(alternative_route, sizeof(alternative_route), "%s%s%s",
+    fileobj->base_route, lastSLash, ".tsv");
+    snprintf(temp, sizeof(temp), "%s%s%s%s", fileobj->base_route,
+    "tsv/", lastSLash, ".tsv");
+    // si no se encuentra la carpeta tsv, crea el archivo tsv en la misma
+    // carpeta donde se encuentra el binario leido
+    fileobj->report_file = fopen(temp, "w+");
+    if (!fileobj->report_file) {
+        printf("Error: could not open tsv file\n");
+        printf("Creating new tsv file\n");
+        fileobj->report_file = fopen(alternative_route, "w+");
+    }
+    snprintf(text, sizeof(temp), "%s\t%" PRIu64 "\t%g\t%g\t%g\t", temp,
+    lamina->time, lamina->conductivity, lamina->height, lamina->epsilon);
+    format_time(lamina->k, text, sizeof(text));
+    fprintf(fileobj->report_file, "%s", text);
+    return EXIT_SUCCESS;
+}
+
 int delete_files(shared_file_data *fileobj) {
     if (fileobj->file) {
         fclose(fileobj->file);
