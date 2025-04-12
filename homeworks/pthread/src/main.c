e7c57a3c359940fc7c2c82874a27d51046c92ffe
Anthony Sanchez <anthony.sanchezquiros@ucr.ac.cr>
2025-04-12 10:11:21 -0600

    Tarea01 and Tarea02:  Adding time measurements

--- homeworks/pthread/src/main.c
+++ homeworks/pthread/src/main.c
@@ -1,10 +1,21 @@
 // Copyright Anthony Sanchez 2024
+
+#define _POSIX_C_SOURCE 199506L
 #include <lamina.h>
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
+    struct timespec start_time;
+    clock_gettime(/*clk_id*/CLOCK_MONOTONIC, &start_time);
     lamina_constructor(argc, argv);
+    struct timespec finish_time;
+    clock_gettime(/*clk_id*/CLOCK_MONOTONIC, &finish_time);
+
+    double elapsed = (finish_time.tv_sec - start_time.tv_sec) +
+      (finish_time.tv_nsec - start_time.tv_nsec) * 1e-9;
+    printf("execution time: %.9lfs\n", elapsed);
     return 0;
 }
