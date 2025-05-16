aa00ee8e3838df496b5ef7f38926732c70319f65
anthony <anthony.sanchezquiros@ucr.ac.cr>
2025-05-16 11:32:08 -0600

    omp_several_for_stages: Finished class example

--- omp/omp_several_for_stages/src/main.cpp
+++ omp/omp_several_for_stages/src/main.cpp
@@ -6,28 +6,38 @@ int main(int argc, char* argv[]) {
   const int thread_count = argc >= 2 ? ::atoi(argv[1]) : omp_get_num_procs();
   const int iteration_count = argc >= 3 ? ::atoi(argv[2]) : thread_count;
   // const int stage_count = argc >= 4 ? ::atoi(argv[3]) : thread_count;
+  #pragma omp parallel num_threads(thread_count) \
+    default(none) shared(std::cout, iteration_count, thread_count)
+  {
+    #pragma omp for  schedule(static,1)
+    for (int iteration = 0; iteration < iteration_count; ++iteration) {
+      #pragma omp critical(print)
+      std::cout << "stage 1: " << omp_get_thread_num() << '/'
+        << omp_get_num_threads() << ": iteration " << iteration << '/'
+        << iteration_count << std::endl;
+    } // #pragma omp barrier
+    #pragma omp single
+    std::cout << /*omp_get_thread_num() <<*/ std::endl;
 
-  for (int iteration = 0; iteration < iteration_count; ++iteration) {
-    std::cout << "stage 1: " << omp_get_thread_num() << '/'
-      << omp_get_num_threads() << ": iteration " << iteration << '/'
-      << iteration_count << std::endl;
+    #pragma omp for  schedule(dynamic,1)
+    for (int iteration = 0; iteration < iteration_count; ++iteration) {
+      #pragma omp critical(print)
+      std::cout << "stage 2: " << omp_get_thread_num() << '/'
+        << omp_get_num_threads() << ": iteration " << iteration << '/'
+        << iteration_count << std::endl;
+    }
+    #pragma omp single
+    std::cout << /*omp_get_thread_num() <<*/ std::endl;
+    
+    #pragma omp for  schedule(static,1)
+    for (int iteration = 0; iteration < iteration_count; ++iteration) {
+      #pragma omp critical(print)
+      std::cout << "stage 3: " << omp_get_thread_num() << '/'
+        << omp_get_num_threads() << ": iteration " << iteration << '/'
+        << iteration_count << std::endl;
+    }
   }
-
-  std::cout << /*omp_get_thread_num() <<*/ std::endl;
-
-  for (int iteration = 0; iteration < iteration_count; ++iteration) {
-    std::cout << "stage 2: " << omp_get_thread_num() << '/'
-      << omp_get_num_threads() << ": iteration " << iteration << '/'
-      << iteration_count << std::endl;
-  }
-
-  std::cout << /*omp_get_thread_num() <<*/ std::endl;
-
-  for (int iteration = 0; iteration < iteration_count; ++iteration) {
-    std::cout << "stage 3: " << omp_get_thread_num() << '/'
-      << omp_get_num_threads() << ": iteration " << iteration << '/'
-      << iteration_count << std::endl;
-  }
+  
 }
 
 // void stages3(const int thread_count, const int iteration_count) {
