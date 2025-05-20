b4ef6434fcb8d42d98173240838744e81c7729a9
anthony <anthony.sanchezquiros@ucr.ac.cr>
2025-05-20 11:53:07 -0600

    omp_stats: reductions except median

--- omp/omp_stats/src/stats.hpp
+++ omp/omp_stats/src/stats.hpp
@@ -5,10 +5,13 @@
 #include <limits>
 #include <cmath>
 #include <vector>
+#include <omp.h>
 
 template <typename Type>
 Type minimum(const std::vector<Type>& values) {
   Type result = std::numeric_limits<Type>::max();
+  #pragma omp parallel for schedule(static) reduction(min:result) \
+    default(none) shared(values)
   for (size_t index = 0; index < values.size(); ++index) {
     if (values[index] < result) {
       result = values[index];
@@ -20,6 +23,8 @@ Type minimum(const std::vector<Type>& values) {
 template <typename Type>
 Type maximum(const std::vector<Type>& values) {
   Type result = std::numeric_limits<Type>::min();
+  #pragma omp parallel for schedule(static) reduction(max:result) \
+    default(none) shared(values)
   for (size_t index = 0; index < values.size(); ++index) {
     if (values[index] > result) {
       result = values[index];
@@ -28,9 +33,30 @@ Type maximum(const std::vector<Type>& values) {
   return result;
 }
 
+/*
 template <typename Type>
 Type sum(const std::vector<Type>& values) {
   Type result = Type();
+  #pragma omp parallel \
+    default(none) shared(result, values)
+  {
+    Type my_private_sum = {};
+    #pragma omp for schedule(static)
+    for (size_t index = 0; index < values.size(); ++index) {
+      my_private_sum += values[index];
+    }
+      #pragma omp atomic
+      result += my_private_sum;
+  }
+  return result;
+}
+*/
+
+template <typename Type>
+Type sum(const std::vector<Type>& values) {
+  Type result = Type();
+  #pragma omp parallel for schedule(static) reduction(+:result) \
+    default(none) shared(values)
   for (size_t index = 0; index < values.size(); ++index) {
     result += values[index];
   }
@@ -52,6 +78,8 @@ double std_dev(const std::vector<Type>& values) {
   if (values.size() >= 2) {
     const double avg = average(values);
     double square_sum = 0.0;
+    #pragma omp parallel for schedule(static) reduction(+:square_sum) \
+    default(none) shared(values, avg)
     for (size_t index = 0; index < values.size(); ++index) {
       square_sum += square(values[index] - avg);
     }
