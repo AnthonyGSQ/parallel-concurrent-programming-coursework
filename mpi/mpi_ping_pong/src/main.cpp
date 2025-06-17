4c2e498f9c019b0bce6660bf1694e16be790f7b8
anthony <anthony.sanchezquiros@ucr.ac.cr>
2025-06-17 11:49:13 -0600

    mpi/mpi_ping_pong: not finished class example

--- mpi/mpi_ping_pong/src/main.cpp
+++ mpi/mpi_ping_pong/src/main.cpp
@@ -4,13 +4,21 @@
 
 #include "Mpi.hpp"
 
-void play_ping_pong(Mpi& mpi);
+void play_ping_pong(Mpi& mpi, const long delay);
 
 int main(int argc, char* argv[]) {
   try {
     Mpi mpi(argc, argv);
+    if (mpi.size() == 2) {
     const long delay = argc >= 2 ? std::stol(argv[1]) : 500;  // milliseconds
+    int myProbability;
+    myProbability = argc >= 3 ?
+      std::stoi(argv[mpi.rank() + 3]) : 50; // percent
     play_ping_pong(mpi, delay);
+    } else if (mpi.rank() == 0) {
+      std:: cerr << "error: only two processes are allowed" << std::endl;
+      std::cerr << "usage: mpiexcec -np 2 bin/mpi_ping_pong" << std::endl;
+    }
   } catch (const Mpi::Error& error) {
     std::cerr << "error: " << error.what() << std::endl;
   }
@@ -18,8 +26,11 @@ int main(int argc, char* argv[]) {
 }
 
 void play_ping_pong(Mpi& mpi, const long delay) {
+  long ball;
   while (true) {
-    std::cout << mpi.rank() << " serves" << std::endl;
+    if () {
+      std::cout << mpi.rank() << ball << std::endl;
     usleep(1000 * delay);
+    }
   }
 }
