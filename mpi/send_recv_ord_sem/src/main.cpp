9751eb77acab845848c4cf140da0e0d9213a0449
anthony <anthony.sanchezquiros@ucr.ac.cr>
2025-06-17 10:39:47 -0600

    send_recv_ord_sem: finished class example

--- mpi/send_recv_ord_sem/src/main.cpp
+++ mpi/send_recv_ord_sem/src/main.cpp
@@ -6,8 +6,16 @@
 int main(int argc, char* argv[]) {
   try {
     Mpi mpi(argc, argv);
+    bool can_greet = false;
+    if (mpi.rank() > 0) {
+      mpi.receive(can_greet, mpi.rank() - 1);
+    }
     std::cout << "Hello from main thread of process " << mpi.rank()
-        << " of " << mpi.size() << " on " << mpi.getHostname() << std::endl;
+      << " of " << mpi.size() << " on " << mpi.getHostname() << std::endl;
+    if (mpi.rank() < mpi.size() - 1) {
+      can_greet = true;
+      mpi.send(can_greet, mpi.rank() + 1);
+    }
   } catch (const std::exception& error) {
     std::cerr << "error: " << error.what() << std::endl;
   }
